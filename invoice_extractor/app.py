d70ff539c0aa717f0cc45b93d9bdf267d50b50f5
Ugo Benassayag <55096113+UgoBena@users.noreply.github.com>
2024-11-25 14:22:13 +0000

    improve app visual

--- invoice_extractor/app.py
+++ invoice_extractor/app.py
@@ -1,6 +1,6 @@
-from typing import Dict
+from pathlib import Path
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 
@@ -8,12 +8,16 @@ from invoice_extractor.extractor import InvoiceExtractor
 
 app = FastAPI()
 
+BASE_DIR = Path(__file__).resolve().parent
 # Serve HTML from "static" directory
-app.mount("/static", StaticFiles(directory="invoice_extractor/static"), name="static")
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 
 
 @app.post("/upload/")
 async def upload_pdf(file: UploadFile = File(...)):
+    if not file.filename.endswith('.pdf'):
+        raise HTTPException(status_code=400, detail="Invalid file format. Only PDFs are allowed.")
+
     file_content = await file.read()
     result = InvoiceExtractor().process(file_content)
     return JSONResponse(content=result)
@@ -21,4 +25,4 @@ async def upload_pdf(file: UploadFile = File(...)):
 
 @app.get("/")
 async def main():
-    return FileResponse("static/index.html")
+    return FileResponse(BASE_DIR / "static/index.html")
